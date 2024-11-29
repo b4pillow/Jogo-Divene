cbcb7a890b2101d4f8e4af6a31909eaceddcff4e
b4pillow <barbara.mariano@escolar.ifrn.edu.br>
2024-11-29 17:56:43 -0300

    Ajuste  de animação e mais level design

--- Assets/Scripts/TutorialMensagem.cs
+++ Assets/Scripts/TutorialMensagem.cs
@@ -4,13 +4,13 @@ using UnityEngine;
 
 public class TutorialMensagem : MonoBehaviour
 {
-    public float radious; // Raio da área de interação
-    public GameObject interact; // Objeto de interface que será ativado
-    public LayerMask playerLayer; // Camada do jogador para detectar colisão
+    public float radious;
+    public GameObject interact;
+    public LayerMask playerLayer;
     private Animator anim;
 
     bool onRadious;
-    bool interactActivated; // Variável para verificar se o objeto já foi ativado
+    bool interactActivated;
 
     private void Start()
     {
@@ -19,30 +19,27 @@ public class TutorialMensagem : MonoBehaviour
 
     private void FixedUpdate()
     {
-        Interact(); // Chamando a função Interact na física
+        Interact();
     }
 
     public void Interact()
     {
-        // Verifica se o jogador está dentro do raio especificado
         Collider2D hit = Physics2D.OverlapCircle(transform.position, radious, playerLayer);
         if(hit != null && !interactActivated)
         {
-            interact.SetActive(true);// Ativa o GameObject de interação
+            interact.SetActive(true);
             onRadious = true;
-            interactActivated = true; // Marca como ativado
-            anim.SetTrigger("FadeIn");
+            interactActivated = true;
+            //anim.SetTrigger("FadeIn");
         }
         else if(hit == null)
         {
             onRadious = false;
-            // Não desativa o objeto `interact` quando o jogador sai da área
         }
     }
 
     private void OnDrawGizmosSelected()
     {
-        // Desenha o raio de detecção no editor para visualização
         Gizmos.color = Color.yellow;
         Gizmos.DrawWireSphere(transform.position, radious);
     }
